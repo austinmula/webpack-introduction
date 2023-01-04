030b3e657517700b6fd265ce8206b9c9d10f08a1
cymula <amula@cytonn.com>
2023-01-04 08:16:13 +0300

    added dev-server and config hot-reload

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,14 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    }
 };
